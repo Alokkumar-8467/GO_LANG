8f2bb6d1e26ac1b4fa07cea9a878ebd5ef1f0740
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-07-23 17:56:53 +0530

    Update main.go

--- 04_var_short_declaration/main.go
+++ 04_var_short_declaration/main.go
@@ -1,3 +1,14 @@
 package main()
 
 import ("fmt")
+
+func main(){
+
+	// in initialization we have to mention the type of variable also.
+	var city string
+	city = "London"
+
+	// if we not assign the type of varaible then it automatically knows that it is string.
+	var town = "Kashi";
+
+}
