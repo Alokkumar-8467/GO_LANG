b22e3c41d4a9105eaa10432e432028d4e5e67267
Alok Kumar <138556514+Alokkumar-8467@users.noreply.github.com>
2026-07-23 17:32:06 +0530

    Update main.go

--- 03_packages_imports/main.go
+++ 03_packages_imports/main.go
@@ -1,7 +1,9 @@
 package main
 
 
-// imports brings external packages into the file that we are using
+// imports brings external packages into the file that we are working on it and
+// where actually we need it.
+
 import (
   "fmt"
 )
